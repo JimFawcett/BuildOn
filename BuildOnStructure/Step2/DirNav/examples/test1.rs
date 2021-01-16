38d8fe43f0c0bbc210e6a6392a2d2c96547bb05d
James W. Fawcett <jfawcett@twcny.rr.com>
2021-01-15 21:05:04 -0500

    finished BuildOnStructure::Step2

---
+++ BuildOnStructure/Step2/DirNav/examples/test1.rs
@@ -0,0 +1,44 @@
+/////////////////////////////////////////////////////////////
+// test1.rs - demonstrates mock DirNav operations          //
+//                                                         //
+// Jim Fawcett, https://JimFawcett.github.io, 15 Jan 2021  //
+/////////////////////////////////////////////////////////////
+
+use dir_nav::*;
+
+/*-----------------------------------------------
+  Type MyApp is a test type that:
+  - is a generic parameter for DirNav<App>
+  - responds to DirNav's events
+  - can be retrieved with DirNav::get_app()
+    and interrogated using its member functions.
+*/
+struct MyApp {}
+
+impl DirEvent for MyApp {
+    fn new() -> Self {
+        MyApp {}
+    }
+    fn do_dir(&mut self, dir:&str) {
+        print!("\n  directory: {:?}", dir);
+    }
+    fn do_file(&mut self, file:&str) {
+        print!("\n    file: {:?}", file);
+    }
+}
+impl MyApp {
+    pub fn show_type(&self) {
+        print!("\n  {:?}", std::any::type_name::<MyApp>());
+    }
+}
+
+fn main() {
+    print!("\n  -- demonstrate mock DirNav --\n");
+
+    let mut dn = DirNav::<MyApp>::new();
+    dn.start();
+    println!();
+    dn.get_app().show_type();
+
+    print!("\n\n  That's all Folks!\n\n");
+}
---
+++ BuildOnStructure/Step2/DirNav/src/lib.rs
@@ -0,0 +1,85 @@
+/////////////////////////////////////////////////////////////
+// Step#2::DirNav::lib.rs - demos mock DirNav operations   //
+//                                                         //
+// Jim Fawcett, https://JimFawcett.github.io, 15 Jan 2021  //
+/////////////////////////////////////////////////////////////
+
+
+/*-----------------------------------------------
+  Rust traits are similar to interfaces.
+
+  Trait DirEvent requires implementer to be
+  constructible and have members to handle
+  DirNav events.
+
+  Constructability allows DirNav to create an
+  instance for a type defined by a generic 
+  parameter.
+*/
+pub trait DirEvent {
+    /*-- constructible --*/
+    fn new() -> Self;
+    /*-- event handlers --*/
+    fn do_dir(&mut self, dir:&str);
+    fn do_file(&mut self, file: &str);
+}
+
+/*-----------------------------------------------
+  App is a type that implements application
+  specific processing of a DirEvent sent by
+  DirNav.  This is a demo for the DirNav library
+  that illustrates how an application can use
+  DirNav.
+  -----------------------------------------------
+pub struct App {
+    curr_dir: String
+}
+impl DirEvent for App {
+    fn new() -> Self {
+        App { curr_dir: String::new() }
+    }
+    fn do_dir(&mut self, dir: &str) {
+        print!("\n  directory: {:?}",dir);
+        self.curr_dir = dir.to_string();
+    }
+    fn do_file(&mut self, file: &str) {
+        print!("\n    file: {:?}", file);
+    }
+}
+*/
+/*-----------------------------------------------
+  DirNav searches directory tree looking for
+  files with specified patterns (extensions).
+
+  This is a mock type defined as an illustration
+  of what Step #2 needs.
+*/
+pub struct DirNav<T> where T:DirEvent {
+    app : T
+}
+impl<T> DirNav<T> where T:DirEvent {
+    pub fn new() -> DirNav<T> {
+        DirNav {
+          app: T::new()
+        }
+    }
+    pub fn get_app(&self) -> &T {
+        &self.app
+    }
+    pub fn start(&mut self) {
+        /* pretending to search a dir tree */
+        self.app.do_dir("dir1");
+        self.app.do_file("file1");
+        self.app.do_file("file2");
+        self.app.do_dir("dir2");
+        self.app.do_file("file3");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
